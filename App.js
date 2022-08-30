8646685dbeadc19e4641289d0628ec7ca4ab6d1f
intersoom <sallybig@naver.com>
2022-08-30 19:26:53 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Button} from 'react-native';
+import Box from './components/Box';
+
+const App = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
+  return (
+    <SafeAreaView>
+      <Button title="토글" onPress={onPress} />
+      {visible && <Box rounded={true} size="large" color="blue" />}
+    </SafeAreaView>
+  );
+};
+
+export default App;
